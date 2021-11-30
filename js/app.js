b60fa26cccb9aceb541473501b6342d8f02a6805
GaboXDPayload <95262472+GaboXDPayload@users.noreply.github.com>
2021-11-29 20:15:58 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js')
+var url = window.location.href;
+var swLocation = '/EXAMEN/sw.js';
 
+
+if (navigator.serviceWorker) {
+    if (url.includes('localhost'))
+        swLocation = '/sw.js';
+
+    navigator.serviceWorker.register(swLocation);
 }
